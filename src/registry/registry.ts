f9f2c1d07b04a31d810d47ed7abdfe7e3e577b22
Luk Fossé <129940537+lukfosse@users.noreply.github.com>
2024-03-04 21:43:08 +0100

    Update registry.ts

--- src/registry/registry.ts
+++ src/registry/registry.ts
@@ -1,5 +1,5 @@
 import bodyParser from "body-parser";
-import express, { Request, Response } from "express";
+import express from "express";
 import { REGISTRY_PORT } from "../config";
 
 export type Node = { nodeId: number; pubKey: string };
@@ -13,13 +13,28 @@ export type GetNodeRegistryBody = {
   nodes: Node[];
 };
 
+
 export async function launchRegistry() {
   const _registry = express();
   _registry.use(express.json());
   _registry.use(bodyParser.json());
 
+  let nodeRegistry: GetNodeRegistryBody = {nodes: []} ;
+
   // TODO implement the status route
-  // _registry.get("/status", (req, res) => {});
+  _registry.get("/status", (req, res) => {
+    res.send("live");
+  });
+
+  _registry.post("/registerNode", (req, res) => {
+    const { nodeId, pubKey } = req.body;
+    nodeRegistry.nodes.push({ nodeId, pubKey });
+    return res.status(200).json({ result: "ok" });
+  });
+
+  _registry.get("/getNodeRegistry", (req, res) => {
+    res.json(nodeRegistry);
+  });
 
   const server = _registry.listen(REGISTRY_PORT, () => {
     console.log(`registry is listening on port ${REGISTRY_PORT}`);
